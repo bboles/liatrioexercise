fc4b1caffa98eab2703b5a5c71799e2a9a026b1d
Brandon Boles <bb@zbeba.com>
2023-04-11 18:53:59 -0700

    Change to disable APIs

--- tf/main.tf
+++ tf/main.tf
@@ -19,30 +19,30 @@ provider "google" {
 resource "google_project_service" "compute" {
   project            = var.project_name
   service            = "compute.googleapis.com"
-  disable_on_destroy = false
+  disable_on_destroy = true
 }
 # Enable the Kubernetes API.
 resource "google_project_service" "container" {
   project            = var.project_name
   service            = "container.googleapis.com"
-  disable_on_destroy = false
+  disable_on_destroy = true
 }
 # Enable the Artifact Registry API.
 resource "google_project_service" "artifactregistry" {
   project            = var.project_name
   service            = "artifactregistry.googleapis.com"
-  disable_on_destroy = false
+  disable_on_destroy = true
 }
 # Enable the Cloud Resource Manager API.
 resource "google_project_service" "crm" {
   project            = var.project_name
   service            = "cloudresourcemanager.googleapis.com"
-  disable_on_destroy = false
+  disable_on_destroy = true
 }
 # Enable the Service Usage API.
 resource "google_project_service" "service_usage" {
   project            = var.project_name
   service            = "serviceusage.googleapis.com"
-  disable_on_destroy = false
+  disable_on_destroy = true
 }
 
