64940342386281eb70176a5bc7cf672ed7318265
Brandon Boles <bb@zbeba.com>
2023-04-13 12:42:45 -0700

    Update outputs.tf

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -23,12 +23,12 @@ output "kubernetes_cluster_host" {
   description = "GKE Cluster Host"
 }
 
-output "pool_name" {
-  description = "Workload Identity Pool name"
-  value       = google_iam_workload_identity_pool.githubpool.name
-}
+# output "pool_name" {
+#  description = "Workload Identity Pool name"
+#  value       = google_iam_workload_identity_pool.githubpool.name
+# }
 
-output "provider_name" {
-  description = "Workload Identity Provider name"
-  value       = google_iam_workload_identity_pool_provider.github.name
-}
+# output "provider_name" {
+#  description = "Workload Identity Provider name"
+#  value       = google_iam_workload_identity_pool_provider.github.name
+#}
